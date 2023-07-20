cdb43e5c8451e3d4acaf746854ac26f28538143f
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-20 11:18:02 +0300

    Add files via upload

---
+++ data_types_and_variables _exercise/integer_operations_v2.py
@@ -0,0 +1,7 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fouth_number = int(input())
+
+rezult = int((first_number + second_number) / third_number) * fouth_number
+print(rezult)

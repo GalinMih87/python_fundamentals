f3e29d6e1e5cfe4da667aa5543e564117dceb16d
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-08-01 10:59:41 +0300

    Add files via upload

---
+++ dictionaries_lab/bakery.py
@@ -0,0 +1,14 @@
+""""
+# for loop
+data = input().split()
+products_dict = {}
+
+for i in range(0, len(data), 2):
+    products_dict[data[i]] = int(data[i + 1])
+print(products_dict)
+"""
+
+# for dict comprehension
+data = input().split()
+products_dict = {data[i]: int(data[i + 1]) for i in range(0, len(data), 2)}
+print(products_dict)
